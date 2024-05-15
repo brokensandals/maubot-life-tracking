09b70561dc1ffa8e7a4c6f1e9f4db0edb17c0cff
Jacob Williams <jacobaw@gmail.com>
2024-05-15 09:46:06 -0700

    more db work

--- maubot_life_tracking/db.py
+++ maubot_life_tracking/db.py
@@ -1,6 +1,10 @@
 from mautrix.util.async_db import UpgradeTable, Connection, Database
 from typing import Optional, List
 from zoneinfo import ZoneInfo
+from datetime import datetime, timedelta, timezone
+
+
+DB_DATETIME_FMT = "%Y-%m-%d %H:%M:%S"
 
 
 upgrade_table = UpgradeTable()
@@ -19,7 +23,7 @@ async def upgrade_v1(conn: Connection) -> None:
           room_id TEXT NOT NULL,
           name TEXT NOT NULL,
           message_template TEXT NOT NULL,
-          next_run DATETIME,
+          next_run_utc TEXT,
           run_interval_sec INTEGER,
           max_random_delay_sec INTEGER,
           PRIMARY KEY (room_id, name),
@@ -32,11 +36,16 @@ class Room:
     def __init__(self, room_id: str, tz: ZoneInfo = None) -> None:
         self.room_id = room_id
         self.tz = tz
-    
-    def tz_str(self) -> str:
-        if self.tz:
-            return self.tz.key
-        return None
+
+
+class Prompt:
+    def __init__(self, room_id: str, name: str, message_template: str, next_run: datetime = None, run_interval: timedelta = None, max_random_delay: timedelta = None) -> None:
+        self.room_id = room_id
+        self.name = name
+        self.message_template = message_template
+        self.next_run = next_run
+        self.run_interval = run_interval
+        self.max_random_delay = max_random_delay
 
 
 async def fetch_room(db: Database, room_id: str) -> Optional[Room]:
@@ -60,4 +69,45 @@ async def upsert_room(db: Database, room: Room) -> None:
         INSERT INTO rooms(id, tz) VALUES ($1, $2)
         ON CONFLICT (id) DO UPDATE SET tz=excluded.tz
     """
-    await db.execute(q, room.room_id, room.tz_str())
+    tzkey = None
+    if room.tz:
+        tzkey = room.tz.key
+    await db.execute(q, room.room_id, tzkey)
+
+
+async def fetch_prompt(db: Database, room_id: str, name: str) -> Optional[Prompt]:
+    q = "SELECT message_template, next_run_utc, run_interval_sec, max_random_delay_sec FROM prompts WHERE room_id=$1 AND name=$2"
+    row = await db.fetchrow(q, room_id, name)
+    if not row:
+        return None
+
+    prompt = Prompt(room_id, name, row["message_template"])
+    if row["next_run_utc"]:
+        prompt.next_run = datetime.strptime(row["next_run_utc"], DB_DATETIME_FMT).replace(tzinfo=timezone.utc)
+    if row["run_interval_sec"]:
+        prompt.run_interval = timedelta(seconds=row["run_interval_sec"])
+    if row["max_random_delay_sec"]:
+        prompt.max_random_delay = timedelta(seconds=row["max_random_delay_sec"])
+    return prompt
+
+
+async def upsert_prompt(db: Database, prompt: Prompt) -> None:
+    q = """
+        INSERT INTO prompts(room_id, name, message_template, next_run_utc, run_interval_sec, max_random_delay_sec) VALUES ($1, $2, $3, $4, $5, $6)
+        ON CONFLICT (room_id, name) DO UPDATE SET message_template=excluded.message_template, next_run_utc=excluded.next_run_utc, run_interval_sec=excluded.run_interval_sec, max_random_delay_sec=excluded.max_random_delay_sec
+    """
+    next_run_utc = None
+    if prompt.next_run:
+        next_run_utc = prompt.next_run.strftime(DB_DATETIME_FMT)
+    run_interval_sec = None
+    if prompt.run_interval:
+        run_interval_sec = prompt.run_interval.total_seconds()
+    max_random_delay_sec = None
+    if prompt.max_random_delay:
+        max_random_delay_sec = prompt.max_random_delay.total_seconds()
+    await db.execute(q, prompt.room_id, prompt.name, prompt.message_template, next_run_utc, run_interval_sec, max_random_delay_sec)
+
+
+async def delete_prompt(db: Database, room_id: str, name: str) -> None:
+    q = "DELETE FROM prompts WHERE room_id=$1 AND name=$2"
+    await db.execute(q, room_id, name)
--- maubot_life_tracking/test_db.py
+++ maubot_life_tracking/test_db.py
@@ -1,8 +1,9 @@
 import unittest
 from pathlib import Path
 from mautrix.util.async_db import Database
-from maubot_life_tracking.db import upgrade_table, fetch_room, upsert_room, Room
+from maubot_life_tracking.db import upgrade_table, fetch_room, upsert_room, Room, Prompt, fetch_prompt, upsert_prompt, delete_prompt
 from zoneinfo import ZoneInfo
+from datetime import datetime, timedelta, timezone
 
 DB_PATH = Path("test.sqlite3")
 DB_FILES = [DB_PATH, Path("test.sqlite3-shm"), Path("test.sqlite3-wal")]
@@ -30,6 +31,33 @@ class TestDb(unittest.IsolatedAsyncioTestCase):
             self.assertEqual(ZONE, room.tz)
 
             self.assertEqual(None, await fetch_room(db, "b"))
+
+            self.assertEqual(None, await fetch_prompt(db, "a", "foo"))
+            prompt = Prompt("a", "foo", "Test Message")
+            await upsert_prompt(db, prompt)
+            prompt = await fetch_prompt(db, "a", "foo")
+            self.assertEqual(prompt.room_id, "a")
+            self.assertEqual(prompt.name, "foo")
+            self.assertEqual(prompt.message_template, "Test Message")
+            self.assertEqual(prompt.next_run, None)
+            self.assertEqual(prompt.run_interval, None)
+            self.assertEqual(prompt.max_random_delay, None)
+
+            now = datetime.now(timezone.utc).replace(microsecond=0)
+            prompt.next_run = now
+            prompt.run_interval = timedelta(days=1)
+            prompt.max_random_delay = timedelta(hours=16)
+            await upsert_prompt(db, prompt)
+            prompt = await fetch_prompt(db, "a", "foo")
+            self.assertEqual(prompt.room_id, "a")
+            self.assertEqual(prompt.name, "foo")
+            self.assertEqual(prompt.message_template, "Test Message")
+            self.assertEqual(prompt.next_run, now)
+            self.assertEqual(prompt.run_interval, timedelta(days=1))
+            self.assertEqual(prompt.max_random_delay, timedelta(hours=16))
+
+            await delete_prompt(db, "a", "foo")
+            self.assertEqual(None, await fetch_prompt(db, "a", "foo"))
         finally:
             await db.stop()
             for path in DB_FILES:
