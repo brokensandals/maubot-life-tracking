5254a078cb622d0d58a6c9eb94635b2767f21add
Jacob Williams <jacobaw@gmail.com>
2024-05-14 21:07:03 -0700

    preliminary files

---
+++ maubot_life_tracking.py
@@ -0,0 +1,32 @@
+from typing import Type
+from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
+from maubot import Plugin, MessageEvent
+from maubot.handlers import event
+from mautrix.types import EventType, MessageEvent
+
+
+class Config(BaseProxyConfig):
+    def do_update(self, helper: ConfigUpdateHelper) -> None:
+        helper.copy("allowlist")
+
+
+class LifeTrackingBot(Plugin):
+    async def start(self) -> None:
+        self.config.load_and_update()
+    
+    def is_allowed(self, sender: str) -> bool:
+        if self.config["allowlist"] == False:
+            return True
+        return sender in self.config["allowlist"]
+
+    @classmethod
+    def get_config_class(cls) -> Type[BaseProxyConfig]:
+        return Config
+
+    @event.on(EventType.ROOM_MESSAGE)
+    async def handle_msg(self, evt: MessageEvent) -> None:
+        if not self.is_allowed(evt.sender):
+            self.log.warn(f"stranger danger: sender={evt.sender}")
+            return
+        pass
+
