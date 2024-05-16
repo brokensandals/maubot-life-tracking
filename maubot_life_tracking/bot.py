e6e063b73453c1a164c42f7c2facefce5594f11c
Jacob Williams <jacobaw@gmail.com>
2024-05-15 17:32:03 -0700

    finish ability to set up prompts

--- maubot_life_tracking/bot.py
+++ maubot_life_tracking/bot.py
@@ -1,4 +1,4 @@
-from typing import Type
+from typing import Type, Optional
 from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
 from maubot import Plugin, MessageEvent
 from maubot.handlers import event, command
@@ -7,6 +7,7 @@ from maubot_life_tracking import db
 from mautrix.util.async_db import UpgradeTable
 from zoneinfo import ZoneInfo
 from datetime import datetime, timezone
+from maubot_life_tracking.parsers import parse_datetime, parse_interval
 
 
 class Config(BaseProxyConfig):
@@ -64,7 +65,7 @@ class LifeTrackingBot(Plugin):
             for prompt in prompts:
                 items.append(f"  - {prompt.name}:")
                 if prompt.next_run:
-                    items.append(f"    - Next run: {prompt.next_run.isoformat()} ({now - prompt.next_run} from now)")
+                    items.append(f"    - Next run: {prompt.next_run.isoformat()} ({prompt.next_run - now} from now)")
                 else:
                     items.append(f"    - Not scheduled")
                 if prompt.run_interval is not None:
@@ -119,6 +120,66 @@ class LifeTrackingBot(Plugin):
         await db.upsert_prompt(self.database, prompt)
         await evt.mark_read()
         await evt.react("✅")
+    
+    @lt_command.subcommand(help="Set the next run date and time, run interval, and random delay for a prompt.")
+    @command.argument("prompt_name")
+    @command.argument("date", required=False)
+    @command.argument("time", required=False)
+    @command.argument("run_interval", required=False)
+    @command.argument("max_random_delay", required=False)
+    async def schedule(self, evt: MessageEvent, prompt_name: str, date: Optional[str], time: Optional[str], run_interval: Optional[str], max_random_delay: Optional[str]) -> None:
+        if not self.is_allowed(evt.sender):
+            self.log.warn(f"stranger danger: sender={evt.sender}")
+            return
+        room = await self.get_room(evt.room_id)
+        prompt = await db.fetch_prompt(self.database, room.room_id, prompt_name)
+        if prompt is None:
+            await evt.mark_read()
+            await evt.reply("You must create the prompt with the !prompt command first.")
+            return
+
+        if date and not time:
+            await evt.mark_read()
+            await evt.reply("You must supply a time if you supply a date.")
+            return
+
+        if not date:
+            prompt.next_run = None
+        else:
+            try:
+                prompt.next_run = parse_datetime(f"{date} {time}", self.get_tz(room))
+            except Exception as e:
+                self.log.warn(e)
+                await evt.mark_read()
+                await evt.reply("Unable to parse date or time. Date should be 'today', 'tomorrow', or 'YYYY-MM-DD'. Time should be 'HH:MM'.")
+                return
+
+        if not run_interval:
+            prompt.run_interval = None
+        else:
+            try:
+                prompt.run_interval = parse_interval(run_interval)
+            except Exception as e:
+                self.log.warn(e)
+                await evt.mark_read()
+                await evt.reply(f"Unable to parse run interval. Expected format is 15d, 15h, or 15m, for 15 days, hours, or minutes, respectively.")
+                return
+
+        if not max_random_delay:
+            prompt.max_random_delay = None
+        else:
+            try:
+                prompt.max_random_delay = parse_interval(max_random_delay)
+            except Exception as e:
+                self.log.warn(e)
+                await evt.mark_read()
+                await evt.reply("Unable to parse max random delay. Expected format is 15d, 15h, or 15m, for 15 days, hours, or minutes, respectively.")
+                return
+        
+        await db.upsert_prompt(self.database, prompt)
+        await evt.mark_read()
+        await evt.react("✅")
+
 
     @event.on(EventType.ROOM_MESSAGE)
     async def handle_msg(self, evt: MessageEvent) -> None:
